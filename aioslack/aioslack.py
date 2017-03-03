1b7657c5a59a3707ff8c931bc1fb98361908cc7e
Joe H. Rahme <jhakimra@redhat.com>
2017-03-03 18:48:35 +0100

    Adds basic repository layout
    * Uses pbr (https://docs.openstack.org/developer/pbr/) to manage
      setuptools
    * makes a module.
    * Apache 2 License
    * Adds a tox file with a style check job

--- aioslack/aioslack.py
+++ aioslack/aioslack.py
@@ -16,10 +16,10 @@ from functools import singledispatch
 logging.basicConfig(level=logging.INFO)
 coloredlogs.install()
 
+
 class Client(object):
-    PRODUCER_DELAY = 0.5 # debounce delay in seconds to avoid overloading the websocket connection
+    PRODUCER_DELAY = 0.5
     BASE_URL = "https://slack.com/api"
-    
 
     def __init__(self, token):
         self.token = token
@@ -30,10 +30,8 @@ class Client(object):
         self.channels = []
         self.users = []
 
-
     def run(self):
         self.loop.run_until_complete(self.start_ws_connection())
-        
 
     def on(self, event, **options):
         def _decorator(callback):
@@ -41,10 +39,8 @@ class Client(object):
             return callback
         return _decorator
 
-
     def unregister(self, event_type, callback):
-        self.handlers[event].remove(callback)
-
+        self.handlers[event_type].remove(callback)
 
     def send_forever(self, request):
         self.requests.append(request)
@@ -55,10 +51,10 @@ class Client(object):
                 if channel['name'] == channel_name:
                     return channel
         else:
-            self.channels = await self.get("channels.list",
-                                           callback=lambda response: response['channels'])
+            self.channels = await self.get(
+                "channels.list",
+                callback=lambda response: response['channels'])
             return self.find_channel(channel_name)
-                
 
     async def find_user(self, user_name):
         if self.users:
@@ -66,10 +62,10 @@ class Client(object):
                 if user['name'] == user_name:
                     return user
         else:
-            self.users = await self.get("users.list",
-                                        callback=lambda response: response['users'])
+            self.users = await self.get(
+                "users.list",
+                callback=lambda response: response['users'])
             return self.find_user(user_name)
-                    
 
     async def get(self, path, extraParams={}, extraHeaders={}, callback=None):
         async def get_request(session, headers, params):
@@ -78,10 +74,8 @@ class Client(object):
                     headers=headers,
                     params=params) as resp:
                 return await self.handle_http_response(resp, path, callback)
-                
-        return await self.make_http_request(get_request, extraParams, extraHeaders)
-
-
+        return await self.make_http_request(
+            get_request, extraParams, extraHeaders)
 
     async def post(self, path, extraData={}, extraHeaders={}, callback=None):
         async def post_request(session, headers, data):
@@ -90,18 +84,15 @@ class Client(object):
                     headers=headers,
                     data=data) as resp:
                 return await self.handle_http_response(resp, path, callback)
-                
-        return await self.make_http_request(post_request, extraData, extraHeaders)
-
-    
-
+        return await self.make_http_request(
+            post_request, extraData, extraHeaders)
 
     async def start_ws_connection(self):
         def retrieve(data):
             return (data["url"], data["channels"], data["users"])
-        
+
         logging.info('handshaking')
-        (url, channels, users) = await self.post("rtm.start", callback=retrieve)
+        url, channels, users = await self.post("rtm.start", callback=retrieve)
         self.channels = channels
         self.users = users
         logging.info('connecting')
@@ -109,19 +100,20 @@ class Client(object):
             logging.info('initializing')
             await self.ws_server_loop(websocket)
 
-
     async def make_http_request(self, request, extraData={}, extraHeaders={}):
-        headers = {'user-agent': 'slackclient/12 Python/3.6.0 Darwin/15.5.0', **extraHeaders}
+        headers = {
+            'user-agent': 'slackclient/12 Python/3.6.0 Darwin/15.5.0',
+            **extraHeaders}
         data = {'token': self.token, **extraData}
 
         async with aiohttp.ClientSession() as session:
             return await request(session, headers, data)
-        
 
     async def handle_http_response(self, resp, req_name, callback=None):
         status = resp.status
         if status != 200:
-            logging.error('{} request was not successful [Code: {}]'.format(req_name, status))
+            logging.error('{} request was not successful [Code: {}]'.format(
+                req_name, status))
             raise Exception('not success')
         else:
             data = await resp.json()
@@ -139,7 +131,6 @@ class Client(object):
             done, pending = await asyncio.wait(
                 [listener_task, producer_task],
                 return_when=asyncio.FIRST_COMPLETED)
-
             if listener_task in done:
                 message = listener_task.result()
                 await self.consumer(message)
@@ -155,14 +146,14 @@ class Client(object):
         for handler in itertools.chain(
                 self.handlers[jsonified['type']],
                 self.handlers['*']):
-            task = asyncio.ensure_future(handler(jsonified))
+            asyncio.ensure_future(handler(jsonified))
 
     async def producer(self):
         if self.requests:
             time.sleep(Client.PRODUCER_DELAY)
             data = await Client.retrieve_data(self.producers.__next__())
             if data:
-                return json.dumps({"id" : uuid.uuid4().int, **data})
+                return json.dumps({"id": uuid.uuid4().int, **data})
 
     @singledispatch
     async def retrieve_data(producer):
@@ -171,7 +162,7 @@ class Client(object):
     @retrieve_data.register(dict)
     async def _(producer):
         return producer
-        
+
     @retrieve_data.register(types.FunctionType)
     async def _(producer):
         return await producer()
